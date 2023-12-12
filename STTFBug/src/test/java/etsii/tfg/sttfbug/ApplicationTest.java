019d76c98299e907c8bc9bf13752051372fc6f15
lNoelia <noelialopezduran@gmail.com>
2023-12-12 23:36:10 +0100

    Initial commit

---
+++ STTFBug/src/test/java/etsii/tfg/sttfbug/ApplicationTest.java
@@ -0,0 +1,13 @@
+package etsii.tfg.sttfbug;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
