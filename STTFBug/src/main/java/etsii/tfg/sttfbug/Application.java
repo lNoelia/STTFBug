9d302f2d417363e441c913f2c486424509507ef1
lNoelia <noelialopezduran@gmail.com>
2024-01-12 20:42:13 +0100

    Select of method to be used on main app

--- STTFBug/src/main/java/etsii/tfg/sttfbug/Application.java
+++ STTFBug/src/main/java/etsii/tfg/sttfbug/Application.java
@@ -1,8 +1,58 @@
 package etsii.tfg.sttfbug;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
+import etsii.tfg.sttfbug.issues.WebScraper;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Integer actionSelected = selectValue();
+        System.out.println("Selected value: "+ actionSelected);
+        switch(actionSelected){
+            case 0: try {
+                    WebScraper.searchDocs();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } break;
+            case 1: try {
+                    WebScraper.searchDocs();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } break;
+            default: throw new IllegalArgumentException("Unexpected value: " + actionSelected);
+        }
     }
+    private static Integer selectValue(){
+        Map<Integer,String> validValues = Map.of(0,"-> Obtain the documents with the list of issues",
+        1,"Obtain de issues of the previous documents",2,"-> This is a preview text");
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Select one of the following values: %n");
+        printValidValues(validValues);
+        Integer actionInteger;
+        while (true) {
+            try {
+                actionInteger = Integer.parseInt(in.nextLine());
+                if (validValues.keySet().contains(actionInteger)) {
+                    break;
+                } else {
+                    System.out.println("The indicated value is not valid, please choose beetween the following list:");
+                    printValidValues(validValues);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please, choose a valid value from the following list:");
+                printValidValues(validValues);
+            }
+        }
+        in.close();
+        return actionInteger;
+        
+    }
+    private static void printValidValues(Map<Integer,String> map){
+        map.entrySet().forEach(entry -> System.out.println(entry.getKey()+" "+entry.getValue()));
+    }
+    
 }
--- STTFBug/src/main/java/etsii/tfg/sttfbug/issues/WebScraper.java
+++ STTFBug/src/main/java/etsii/tfg/sttfbug/issues/WebScraper.java
@@ -1,30 +1,62 @@
 package etsii.tfg.sttfbug.issues;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 
 public class WebScraper{
-    public static void main(String[] args) {
-        List<Document> lDocuments = searchDocs();
-        ArrayList<Issue> lIssues = new ArrayList<>();
-        for(Document doc:lDocuments){
-            lIssues.addAll(getListIssues(doc));
-        }
-    }
+    private static final String LRF_FILE_PATH = "../../../../../../../resources/LRF.html";//Html file of the List of resolved and fixed issues
+    private static final String LVF_FILE_PATH = "../../../../../../../LVF.html";//Html file of the List of verified and fixed issues
+    private static final String LCF_FILE_PATH = "../../../../../../../LCF.html";//Html file of the List of closed and fixed issues
+    private static final List<String> lfiles= List.of(LRF_FILE_PATH,LVF_FILE_PATH,LCF_FILE_PATH);
     
-    public static List<Document> searchDocs(){
+    /**
+     * @return a list of the Jsoup document of the different bug list 
+     */
+    public static List<Document> searchDocs() throws IOException{
         //TO-DO Implement condition to check if the HTML is available on local, if it isn't, then use Jsoup.connect
+
         List<Document> res = new ArrayList<>();
-        try {
-            Document doc = Jsoup.connect("https://bugs.eclipse.org/bugs/report.cgi?bug_file_loc=&bug_file_loc_type=allwordssubstr&bug_id=&bug_id_type=anyexact&chfieldfrom=&chfieldto=Now&chfieldvalue=&email1=&email2=&email3=&emailtype1=substring&emailtype2=substring&emailtype3=substring&field0-0-0=noop&keywords=&keywords_type=allwords&longdesc=&longdesc_type=allwordssubstr&short_desc=&short_desc_type=allwordssubstr&status_whiteboard=&status_whiteboard_type=allwordssubstr&type0-0-0=noop&value0-0-0=&votes=&votes_type=greaterthaneq&x_axis_field=resolution&y_axis_field=bug_status&z_axis_field=&width=600&height=350&action=wrap&format=table&product=Platform").get();
-            res.add(getResolvedIssuesDocument(doc)); //RF: Resolved Fixed
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        for(String f:lfiles){
+            File file = new File(f);
+            if (file.exists()) {//If we already have this file, we dont need to look for it again
+                res.add(Jsoup.parse(file, "UTF-8"));
+            }
+            try {
+                Document doc = Jsoup.connect("https://bugs.eclipse.org/bugs/report.cgi?bug_file_loc=&bug_file_loc_type=allwordssubstr&bug_id=&bug_id_type=anyexact&chfieldfrom=&chfieldto=Now&chfieldvalue=&email1=&email2=&email3=&emailtype1=substring&emailtype2=substring&emailtype3=substring&field0-0-0=noop&keywords=&keywords_type=allwords&longdesc=&longdesc_type=allwordssubstr&short_desc=&short_desc_type=allwordssubstr&status_whiteboard=&status_whiteboard_type=allwordssubstr&type0-0-0=noop&value0-0-0=&votes=&votes_type=greaterthaneq&x_axis_field=resolution&y_axis_field=bug_status&z_axis_field=&width=600&height=350&action=wrap&format=table&product=Platform").get();
+                switch (f) {
+                    case LRF_FILE_PATH:
+                        Document lrfdoc = getResolvedIssuesDocument(doc);
+                        generateDoc(lrfdoc, file);
+                        res.add(lrfdoc);
+                        break;
+                    case LVF_FILE_PATH:
+                        //ToDo 
+                        break;
+                    case LCF_FILE_PATH:
+                        //ToDo
+                        break;
+                    default:
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    
         return res;
+    }
+
+    /**
+     * @param ldoc List of documents we are going to use to get the issues
+     */
+    public static void getListAllIssues(List<Document> ldoc){
+            System.out.println("a");
     }
     /**
      * @param doc Document of the Report: Status/Solution page 
@@ -60,4 +92,15 @@ public class WebScraper{
         return null;
     }
 
+    private static void generateDoc(Document doc, File file){
+        doc.outputSettings().charset("UTF-8");
+        doc.outputSettings().prettyPrint(false);
+        doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+        doc.outputSettings().escapeMode(null);
+        try {
+            FileUtils.writeStringToFile(file, doc.outerHtml(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
