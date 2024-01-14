8701d044cbe55e585b6d3332681e14e94d62b5eb
lNoelia <noelialopezduran@gmail.com>
2024-01-14 13:46:01 +0100

    Downloading CSV instead of using Jsoup document for issue list

--- STTFBug/src/main/java/etsii/tfg/sttfbug/Application.java
+++ STTFBug/src/main/java/etsii/tfg/sttfbug/Application.java
@@ -1,6 +1,7 @@
 package etsii.tfg.sttfbug;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -10,32 +11,34 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Integer actionSelected = selectValue();
-        System.out.println("Selected value: "+ actionSelected);
-        switch(actionSelected){
-            case 0: try {
-                    WebScraper.searchDocs();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } break;
-            case 1: try {
-                    WebScraper.searchDocs();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } break;
-            default: throw new IllegalArgumentException("Unexpected value: " + actionSelected);
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            Integer actionSelected = selectValue(scanner);
+            System.out.println("Selected value: "+ actionSelected);
+            switch(actionSelected){
+                case 0: System.out.println("Stopping execution");
+                        scanner.close();
+                        return;
+                case 1: try {
+                        WebScraper.searchDocs();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } break;
+                default: throw new IllegalArgumentException("Unexpected value: " + actionSelected);
+            }
         }
     }
-    private static Integer selectValue(){
-        Map<Integer,String> validValues = Map.of(0,"-> Obtain the documents with the list of issues",
-        1,"Obtain de issues of the previous documents",2,"-> This is a preview text");
-        Scanner in = new Scanner(System.in);
+    private static Integer selectValue(Scanner scanner){
+        Map<Integer,String> validValues = Map.of(0,"-> Stop execution",
+        1,"-> Obtain a .csv file with the list of issues",
+        2,"-> Use the .csv files obtained to get the issues",
+        3,"-> This is a preview text");
         System.out.printf("Select one of the following values: %n");
         printValidValues(validValues);
         Integer actionInteger;
         while (true) {
             try {
-                actionInteger = Integer.parseInt(in.nextLine());
+                actionInteger = Integer.parseInt(scanner.nextLine());
                 if (validValues.keySet().contains(actionInteger)) {
                     break;
                 } else {
@@ -47,12 +50,13 @@ public class Application {
                 printValidValues(validValues);
             }
         }
-        in.close();
         return actionInteger;
         
     }
     private static void printValidValues(Map<Integer,String> map){
-        map.entrySet().forEach(entry -> System.out.println(entry.getKey()+" "+entry.getValue()));
+        map.entrySet().stream()
+        .sorted(Comparator.comparing(Map.Entry::getKey))
+        .forEach(entry -> System.out.println(entry.getKey()+" "+entry.getValue()));
     }
     
 }
--- STTFBug/src/main/java/etsii/tfg/sttfbug/issues/WebScraper.java
+++ STTFBug/src/main/java/etsii/tfg/sttfbug/issues/WebScraper.java
@@ -1,7 +1,8 @@
 package etsii.tfg.sttfbug.issues;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -9,47 +10,42 @@ import org.jsoup.*;
 import org.jsoup.nodes.*;
 
 public class WebScraper{
-    private static final String LRF_FILE_PATH = "../../../../../../../resources/LRF.html";//Html file of the List of resolved and fixed issues
-    private static final String LVF_FILE_PATH = "../../../../../../../LVF.html";//Html file of the List of verified and fixed issues
-    private static final String LCF_FILE_PATH = "../../../../../../../LCF.html";//Html file of the List of closed and fixed issues
+    private static final String MAIN_URL = "https://bugs.eclipse.org/bugs/report.cgi?x_axis_field=resolution&y_axis_field=bug_status&z_axis_field=&width=600&height=350&action=wrap&format=table&product=Platform";
+    private static final String LRF_FILE_PATH = "resources/LRF.csv";//File with a list of resolved and fixed issues
+    private static final String LVF_FILE_PATH = "resources/LVF.csv";//File with a list of verified and fixed issues
+    private static final String LCF_FILE_PATH = "resources/LCF.csv";//File with a list of closed and fixed issues
     private static final List<String> lfiles= List.of(LRF_FILE_PATH,LVF_FILE_PATH,LCF_FILE_PATH);
-    
+
+    public static void main(String[] args) {
+        try {
+            searchDocs();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     /**
-     * @return a list of the Jsoup document of the different bug list 
+     * @return A list of the Jsoup document(HTML form) of the different bug list 
      */
-    public static List<Document> searchDocs() throws IOException{
-        //TO-DO Implement condition to check if the HTML is available on local, if it isn't, then use Jsoup.connect
-
-        List<Document> res = new ArrayList<>();
-
+    public static void searchDocs() throws IOException{
         for(String f:lfiles){
             File file = new File(f);
-            if (file.exists()) {//If we already have this file, we dont need to look for it again
-                res.add(Jsoup.parse(file, "UTF-8"));
-            }
-            try {
-                Document doc = Jsoup.connect("https://bugs.eclipse.org/bugs/report.cgi?bug_file_loc=&bug_file_loc_type=allwordssubstr&bug_id=&bug_id_type=anyexact&chfieldfrom=&chfieldto=Now&chfieldvalue=&email1=&email2=&email3=&emailtype1=substring&emailtype2=substring&emailtype3=substring&field0-0-0=noop&keywords=&keywords_type=allwords&longdesc=&longdesc_type=allwordssubstr&short_desc=&short_desc_type=allwordssubstr&status_whiteboard=&status_whiteboard_type=allwordssubstr&type0-0-0=noop&value0-0-0=&votes=&votes_type=greaterthaneq&x_axis_field=resolution&y_axis_field=bug_status&z_axis_field=&width=600&height=350&action=wrap&format=table&product=Platform").get();
+            Files.deleteIfExists(file.toPath());// To avoid unique name issues, we delete the .csv if it already exists 
+            Document doc = Jsoup.connect(MAIN_URL).get();
                 switch (f) {
                     case LRF_FILE_PATH:
-                        Document lrfdoc = getResolvedIssuesDocument(doc);
-                        generateDoc(lrfdoc, file);
-                        res.add(lrfdoc);
+                        getResolvedIssuesDocument(doc);
                         break;
                     case LVF_FILE_PATH:
-                        //ToDo 
+                        getVerifiedIssuesDocument(doc);
                         break;
                     case LCF_FILE_PATH:
-                        //ToDo
+                        getClosedIssuesDocument(doc);
                         break;
                     default:
                         break;
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     
-        return res;
+        }
     }
 
     /**
@@ -60,23 +56,50 @@ public class WebScraper{
     }
     /**
      * @param doc Document of the Report: Status/Solution page 
-     * @return The document with ALL the issues marked as resolved and fixed
      */
-    private static Document getResolvedIssuesDocument(Document doc){
+    private static void getResolvedIssuesDocument(Document doc){
         //Documents.getElementById is not able to search for an ID inside of a table, so we are going to do it step-by-step
         Element table = doc.select("table").first().select("table").first().select("tbody").get(1);
         Element resolvedFixedCell = table.select("tr").get(4).select("td").get(2);//4=Resolved ,2=Fixed
         String auxLink = resolvedFixedCell.select("a").first().attr("abs:href");
-        Document auxDoc = tryConnection(auxLink);//This is the document with the first 1000 issues, but we need to see all the issues.
-        String link = auxDoc.getElementsByClass("bz_result_count").first().select("a").first().attr("abs:href");
-        Document res = tryConnection(link);// Document with all the issues marked as resolved and fixed.
-        System.out.println(res.select("table").first().select("tbody").first().text());//TEST
-        return res;
+        Document auxDoc = tryConnection(auxLink);
+        String link = auxDoc.getElementsByClass("buglist_menu").first().getElementsByClass("bz_query_links").first().select("a").first().attr("abs:href");
+        String downloadPath = LRF_FILE_PATH;
+        downloadCSV(link, downloadPath);
+    }
+
+    private static void getVerifiedIssuesDocument(Document doc){
+        Element table = doc.select("table").first().select("table").first().select("tbody").get(1);
+        Element verifiedFixedCell = table.select("tr").get(5).select("td").get(2);//5=Resolved ,2=Fixed
+        String auxLink = verifiedFixedCell.select("a").first().attr("abs:href");
+        Document auxDoc = tryConnection(auxLink);
+        String link = auxDoc.getElementsByClass("buglist_menu").first().getElementsByClass("bz_query_links").first().select("a").first().attr("abs:href");      
+        String downloadPath = LVF_FILE_PATH;
+        downloadCSV(link, downloadPath);
+    }
+
+    private static void getClosedIssuesDocument(Document doc){
+        Element table = doc.select("table").first().select("table").first().select("tbody").get(1);
+        Element closedFixedCell = table.select("tr").get(6).select("td").get(2);//6=Closed ,2=Fixed
+        String auxLink = closedFixedCell.select("a").first().attr("abs:href");
+        Document auxDoc = tryConnection(auxLink);
+        String link = auxDoc.getElementsByClass("buglist_menu").first().getElementsByClass("bz_query_links").first().select("a").first().attr("abs:href");      
+        String downloadPath = LCF_FILE_PATH;
+        downloadCSV(link, downloadPath);
+    }
+
+    private static void downloadCSV(String url, String path){
+        try {
+            FileUtils.copyURLToFile(new URL(url), new File(path), 90*1000, 90*1000);
+        } catch (Exception e) {
+            System.err.println("An error has occured while downloading the file: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
     private static Document tryConnection(String link){
         try{
-            //On the connection we will use a timeout of 75 second because when we are loading the resolved issues, we are loading more than 37k issues
-            return Jsoup.connect(link).timeout(75*1000).get();
+            //On the connection we will use a timeout of 90 second because when we are loading the resolved issues, we are loading more than 37k issues
+            return Jsoup.connect(link).timeout(90*1000).get();
         } catch (IOException e) {
             System.err.println("No se pudo conectar a " + link + ": " + e.getMessage());
             return new Document("");
@@ -85,22 +108,5 @@ public class WebScraper{
     // private static Issue getIssue(Document doc){
     //     return null;
     // }
-    private static ArrayList<Issue> getListIssues(Document doc) {
-        Element lIssues = doc.select("table").first();//.select("tbody").first();
-        System.out.println(doc.title());
-        System.out.println(lIssues.text());
-        return null;
-    }
 
-    private static void generateDoc(Document doc, File file){
-        doc.outputSettings().charset("UTF-8");
-        doc.outputSettings().prettyPrint(false);
-        doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
-        doc.outputSettings().escapeMode(null);
-        try {
-            FileUtils.writeStringToFile(file, doc.outerHtml(), "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
