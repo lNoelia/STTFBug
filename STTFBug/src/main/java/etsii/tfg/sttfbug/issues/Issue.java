cf6f4678da6ea316887ec52f110c5fc8aaa69b8c
lNoelia <noelialopezduran@gmail.com>
2024-01-23 21:48:46 +0100

    Cleaning the issue class and formatters

--- STTFBug/src/main/java/etsii/tfg/sttfbug/issues/Issue.java
+++ STTFBug/src/main/java/etsii/tfg/sttfbug/issues/Issue.java
@@ -6,12 +6,14 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Issue {
-    private String title;
     private Integer id;
+    private String title;
     private String description;
     private ZonedDateTime startDate;
     private ZonedDateTime endDate;
     private String assignee;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z");
     
     public Integer getId() {
         return id;
@@ -25,17 +27,23 @@ public class Issue {
     public ZonedDateTime getStartDate() {
         return startDate;
     }
+    public String getStartDatetoString() {
+        return startDate.format(formatter);
+    }
     public ZonedDateTime getEndDate() {
         return endDate;
+    }
+    public String getEndDatetoString() {
+        return endDate.format(formatter);
     }
     public String getAssignee() {
         return assignee;
     }
     public void setTitle(String title) {
-        this.title = title;
+        this.title = cleanText(title);
     }
     public void setDescription(String description) {
-        this.description = description;
+        this.description = cleanText(description);
     }
     public void setStartDateStr(String startDate) {
        this.startDate = convertDate(startDate);
@@ -54,18 +62,16 @@ public class Issue {
     }
 
     public ZonedDateTime convertDate(String strDate){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z");
         LocalDateTime date = LocalDateTime.parse(strDate, formatter);
         ZoneId zoneNY = ZoneId.of("America/New_York");
         return ZonedDateTime.of(date, zoneNY);
     }
-
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((title == null) ? 0 : title.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
         result = prime * result + ((description == null) ? 0 : description.hashCode());
         result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
         result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
@@ -81,15 +87,15 @@ public class Issue {
         if (getClass() != obj.getClass())
             return false;
         Issue other = (Issue) obj;
-        if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
         if (id == null) {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
             return false;
         if (description == null) {
             if (other.description != null)
@@ -113,12 +119,13 @@ public class Issue {
             return false;
         return true;
     }
-        @Override
-        public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z");
-
-        return "Issue [title=" + title + ", id=" + id + ", description=" + description + ", startDate=" + (startDate != null ? startDate.format(formatter) : null)
-            + ", endDate=" + (endDate != null ? endDate.format(formatter) : null) + ", assignee=" + assignee + "]";
-        }
+    @Override
+    public String toString() {
+        return "Issue [id=" + id + ", title=" + title + ", description=" + description + ", startDate=" + (startDate != null ? startDate.format(formatter) : null)
+        + ", endDate=" + (endDate != null ? endDate.format(formatter) : null) + ", assignee=" + assignee + "]";
+    }
     
+    private static String cleanText(String text) {
+        return text.replaceAll("\\r?\\n", " ");
+    }
 }
